30dc6be5d3404b16d5b947f8a8be54e82205ee2e
ian <i@u1t.dev>
2025-10-03 12:19:54 -0400

    update webserver

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ import errorHtml from "./errorHtml.js";
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const jsStaticPath = path.join(__dirname, "public", "js");
+const jsStaticRoutes = fs.readdirSync(jsStaticPath);
+const jsStaticSet = new Set(jsStaticRoutes);
+
+const cssStaticPath = path.join(__dirname, "public", "css");
+const cssStaticRoutes = fs.readdirSync(cssStaticPath);
+const cssStaticSet = new Set(cssStaticRoutes);
+
 const server = http.createServer();
 server.on("request", async (req, res) => {
   const q = req.url ? req.url : "";
@@ -27,6 +35,52 @@ server.on("request", async (req, res) => {
       const e = errorHtml(404, `file not found`);
       res.writeHead(404, {
         "content-type": "text/html",
+        "content-encoding": "utf-8",
+        "content-length": e.length,
+      });
+      res.write(e);
+      return res.end();
+    }
+  } else if (q.startsWith("/js/")) {
+    const file = q.replace("/js/", "");
+    try {
+      if (!jsStaticSet.has(file)) throw new Error(`file no in js ${file}`);
+      const route = path.join(jsStaticPath, file);
+      const data = await fs.promises.readFile(route, "utf8");
+      res.writeHead(200, {
+        "content-type": "text/javascript",
+        "content-encoding": "utf-8",
+        "content-length": data.length,
+      });
+      res.write(data);
+      return res.end();
+    } catch (error) {
+      const e = errorHtml(404, `/* file not found */`);
+      res.writeHead(404, {
+        "content-type": "text/javascript",
+        "content-encoding": "utf-8",
+        "content-length": e.length,
+      });
+      res.write(e);
+      return res.end();
+    }
+  } else if (q.startsWith("/css/")) {
+    const file = q.replace("/css/", "");
+    try {
+      if (!cssStaticSet.has(file)) throw new Error(`file no in css ${file}`);
+      const route = path.join(cssStaticPath, file);
+      const data = await fs.promises.readFile(route, "utf8");
+      res.writeHead(200, {
+        "content-type": "text/css",
+        "content-encoding": "utf-8",
+        "content-length": data.length,
+      });
+      res.write(data);
+      return res.end();
+    } catch (error) {
+      const e = errorHtml(404, `/* file not found */`);
+      res.writeHead(404, {
+        "content-type": "text/css",
         "content-encoding": "utf-8",
         "content-length": e.length,
       });
